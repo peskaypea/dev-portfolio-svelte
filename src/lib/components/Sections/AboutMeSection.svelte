e100d30805ea8ded890e9511d7f56c5bdc988feb
Zane Reid <zanecreid@hotmail.com>
2025-06-10 19:40:05 +1000

    About me section create

--- src/lib/components/Sections/AboutMeSection.svelte
+++ src/lib/components/Sections/AboutMeSection.svelte
@@ -1,3 +1,41 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { SectionHeadline, Button } from "$components";
+  import image from "$assets/about-me.jpeg";
+
+  function onclick(){
+    goto("/#contact-form")
+  }
+</script>
+
+<section class="about-me mt-l">
+  <SectionHeadline sectionName={"about-me"}>About Me</SectionHeadline>
+  <div class="mt-m content-container default-margin">
+    <img class="image" src={image} alt="" />
+    <div class="text">
+      <p>
+        Hi, I’m Zane — a Full-Stack developer and designer with a passion for
+        creating fast, functional, and visually compelling websites. I’ve spent
+        the past 3+ years building and refining digital experiences that not
+        only look great but convert users into customers.
+      </p>
+      <p>
+        With a foundation in HTML, SCSS, JavaScript, and Svelte, I build
+        responsive, mobile-first interfaces that are smooth, interactive, and
+        easy to maintain. I also bring a strong design eye and a love for clean,
+        reusable code—whether I’m animating with GSAP, working in a Bootstrap
+        environment, or setting up modern stacks with Vite.
+      </p>
+      <p>
+        When I'm not coding, I'm usually in the ocean — surfing, exploring, or
+        working on creative side projects that blend tech, freedom, and
+        community.
+      </p>
+    </div>
+  </div>
+  <Button className="mt-m" {onclick}>Tell me about your porject</Button>
+</section>
+
 <style>
   .content-container {
     display: flex;
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,4 @@
 export {default as HeroSection} from '../components/Sections/HeroSection.svelte';
 export {default as Button} from '../components/Button.svelte';
+export {default as SectionHeadline} from '../components/SectionHeadline.svelte';
+export {default as AboutMeSection} from '../components/Sections/AboutMeSection.svelte';

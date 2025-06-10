b3050cde8ff016b77937be31325e239b29de848c
Zane Reid <zanecreid@hotmail.com>
2025-06-10 19:20:37 +1000

    added in Hero section

---
+++ src/lib/components/Sections/HeroSection.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+  import {Button} from '$components';
+
+  function onclick(){
+    goto('/#contact-form')
+    console.log('clicked')
+  }
+</script>
+
+<section class="hero-section default-margin">
+<h3>Hello I'm Zane</h3>
+<div class="underscore mb-m"></div>
+<h1 class="">I'm a Full-Stack developer crafing intuitive and user-friendly</h1>
+<h1 class="dark-grey mb-m">web experiences.</h1>
+<Button {onclick}>Let's Talk</Button>
+</section>
+
+
+
+<style>
+  .hero-section {
+    padding-top: 60px;
+  }
+</style>
---
+++ src/lib/components/index.ts
@@ -0,0 +1,2 @@
+export {default as HeroSection} from '../components/Sections/HeroSection.svelte';
+export {default as Button} from '../components/Button.svelte';
